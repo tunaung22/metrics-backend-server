f3900cd77b7e9bcc499ff40f5a39c7ac1ca7f325
Tun Tun Aung <tunaung2048@gmail.com>
2025-05-05 17:37:00 +0700

    - user registration can select multiple roles. - **note role can be omitted when adding user.

--- src/Metrics.Web/Pages/Employee/Success.cshtml.cs
+++ src/Metrics.Web/Pages/Employee/Success.cshtml.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
 
 namespace Metrics.Web.Pages.Employee;
 
@@ -7,13 +8,15 @@ public class SuccessModel : PageModel
 {
     public string? Username { get; set; } = string.Empty;
     public string? FullName { get; set; } = string.Empty;
-    public string? RoleName { get; set; } = string.Empty;
+    public List<string> AssignedRoles { get; set; } = [];
 
     public IActionResult OnGet()
     {
+        var rolesJson = TempData["AssignedRoles"] as string;
+
+        AssignedRoles = (rolesJson != null ? JsonConvert.DeserializeObject<List<string>>(rolesJson) : new List<string>()) ?? [];
         Username = TempData["Username"] as string;
         FullName = TempData["FullName"] as string;
-        RoleName = TempData["RoleName"] as string;
 
         return Page();
     }
