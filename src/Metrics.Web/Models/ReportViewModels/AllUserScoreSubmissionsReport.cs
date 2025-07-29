48fadabb15f18fe4b9392b238645f7c54ea68127
Tun Tun Aung <tunaung2048@gmail.com>
2025-07-29 14:32:28 +0700

    Report Page
    - Department kpi report page redesign
    - Updated css styling

--- src/Metrics.Web/Models/ReportViewModels/AllUserScoreSubmissionsReport.cs
+++ src/Metrics.Web/Models/ReportViewModels/AllUserScoreSubmissionsReport.cs
@@ -6,7 +6,7 @@ public class AllUserScoreSubmissionsReport
 {
     public string? PeriodName { get; set; }
     public string? DepartmentName { get; set; }
-    public List<UserGroupSubmissionInfo> UserGroupSubmissionInfos { get; set; } = [];
+    public List<UserGroupSubmissionInfoViewModel> UserGroupSubmissionInfos { get; set; } = [];
     public long TotalSubmissions { get; set; }
     public decimal TotalScore { get; set; }
     public decimal KpiScore { get; set; }
